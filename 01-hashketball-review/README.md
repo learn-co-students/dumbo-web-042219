ea0605de1e1b7ed080b1607da8a5e86c39781df2
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-04-22 09:26:30 -0400

    init

---
+++ 01-hashketball-review/README.md
@@ -0,0 +1,19 @@
+# Hashketball Review
+
+## Key Questions
+* What's cool about `rspec --fail-fast`?
+* How can I tell an array from a hash in ruby?
+* How should I handle an array differently than a hash in ruby?
+* If I forget what I can do with an array or a hash in ruby, what should I do?
+* When should I use these methods? What can I call them on? What do they return? What do I put in the block?
+  * `#each`
+  * `#map`
+  * `#select`
+  * `#find`
+* What is the Single Responsibility Principle?
+* How can I use the tests to guide me through the labs with maximum learning?
+  * What is Red, Green, Refactor, and how can I use that process?
+
+
+Make it work (shameless green), make it right, make it fast.
+
---
+++ 01-hashketball-review/hashketball.rb
@@ -0,0 +1,155 @@
+require 'pry'
+
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        }, {
+          player_name: "Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        }, {
+          player_name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        }, {
+          player_name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        }, {
+          player_name: "Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        {
+          player_name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        }, {
+          player_name: "Bismak Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        }, {
+          player_name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        }, {
+          player_name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        }, {
+          player_name: "Brendan Haywood",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      ]
+    }
+  }
+end
+
+
+# # EXERCISE:
+# # Define a method that takes an array of instructors...
+# # Group 1: ...named get_names that returns just their names.
+# # Group 2: ...named get_hometowns that returns an array of just the instructors from Illinois.
+# # Group 3: ..named get_names that returns just the first instructor from Illinois.
+
+# instructors = [
+#   {name: 'Graham', hometown: 'Oak Park, IL (outside of Chicago)', mood: 'excited'},
+#   {name: 'Helen', hometown: 'Chicago, IL', mood: 'sleepy' },
+#   {name: 'Dan', hometown: ''}
+# ]
+
+# def method_name(instructor_array)
+
+# end
+
+# method_name(instructors)
+
+
+# # EXERCISE
+# # What do the following return?
+#
+# arr = (1..100).to_a
+#
+# arr.map do |num|
+#   num.even?
+# end
+#
+# arr.select do |num|
+#   7
+# end
+
+
