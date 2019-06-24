ef42ee10a7465609f252ea8bc8fd0092e3974836
Tim Campbell <timothylevicampbell@gmail.com>
2019-06-24 13:45:45 -0400

    React practice intro

---
+++ 35-practice-react-intro/README.md
@@ -0,0 +1,15 @@
+# Lutrivia
+
+You are going to be building Lutrivia! This is intended to be a a bare bones project that is designed to have you practice passing props down from the App component to your other components.
+
+FIRST, come up with a component hierarchy that makes sense and format your data in a way that also makes sense.
+
+SECOND, build out a static version of this. When you are finished your app should look like the image below.
+
+Good luck. When you are finished, jump back on labs!
+
+## Wireframe
+
+![Lutrivia wireframes](./wireframe.png)
+
+## Component hierarchy
---
+++ 35-practice-react-intro/data.js
@@ -0,0 +1,44 @@
+export default {
+    questions: [
+        {
+            text: "An otter's den is called a sofa.",
+            answer: false
+        },
+        {
+            text: "The word otter is related to the English word water.",
+            answer: true
+        },
+        {
+            text: "Otter pups stay with their families for up to one year.",
+            answer: true
+        },
+        {
+            text: "Otters stay warm through a combination of blubber and fur.",
+            answer: false
+        },
+        {
+            text: "Otters commonly engage in playful behaviors.",
+            answer: true
+        },
+        {
+            text: "There are only three existing otter species: sea otter, ",
+            answer: false
+        },
+        {
+            text: "Fish is a staple of the otter diet.",
+            answer: true
+        },
+        {
+            text: "The otter gestation period is between 60 to 86 days.",
+            answer: true
+        },
+        {
+            text: "Otters live all over the world.",
+            answer: true
+        },
+        {
+            text: "Otters show up in myths from different cultures.",
+            answer: true
+        }
+    ]
+}
